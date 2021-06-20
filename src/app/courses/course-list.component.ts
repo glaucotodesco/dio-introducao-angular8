755ffe9a848211b62dcbd29f6242cf6997bd9d55
Glauco Todesco <glauco.todesco@gmail.com>
2021-06-20 14:40:51 -0300

    Aula02-O que é injecaoo de dependencia

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -9,29 +10,13 @@ export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
 
+
+    constructor(private courseService: CourseService){
+        this.courses = courseService.retrieveAll();
+    }
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular 8 Course Basic',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8788',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 1, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular 8 Course Advanced',
-                imageUrl: '/assets/images/http.png',
-                price: 199.99,
-                code: 'XPS-8288',
-                duration: 200,
-                rating: 3.5,
-                releaseDate: 'December, 2, 2020'
-            }
-        ]
+       
     }
 
 
--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -7,4 +7,5 @@ export interface Course {
     duration: number;
     rating: number;
     releaseDate: string;
+    description: string;
 }
