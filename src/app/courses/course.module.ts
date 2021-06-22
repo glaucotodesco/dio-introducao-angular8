99b0a8c21de6ca937e0cb240fc683c086f706906
Glauco Todesco <glauco.todesco@gmail.com>
2021-06-22 17:34:52 -0300

    Aula03-Conhecendo a pasta shared

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star/star.module";
+import { AppPipeModule } from "../shared/pipe/app-pipe.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 
@@ -12,12 +11,12 @@ import { CourseListComponent } from "./course-list.component";
     declarations: [
         CourseInfoComponent,
         CourseListComponent,
-        StarComponent,
-        ReplacePipe,
     ],
     imports: [
         FormsModule,
+        StarModule,
         CommonModule,
+        AppPipeModule,
         RouterModule.forRoot([
             {
                 path: 'courses', component: CourseListComponent
